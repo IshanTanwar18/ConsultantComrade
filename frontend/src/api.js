c7d999554d8a3b451450854f3b5b755a9e680423
IshanTanwar18 <tanwarishan07@gmail.com>
2025-11-18 01:08:42 +0530

    third commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api"; // change to deployed URL later
+const API_BASE_URL = "https://consultantcomrade-bkd.onrender.com"; // change to deployed URL later
 
 export const api = axios.create({
   baseURL: API_BASE_URL
